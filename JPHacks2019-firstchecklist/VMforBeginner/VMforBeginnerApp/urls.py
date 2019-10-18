87af5057698ce9b81db33b1ab2971598605e8f6f
Hirotane <56142746+Hirotane@users.noreply.github.com>
2019-10-18 19:12:16 +0900

    Add files via upload
    demo3 add to home1

---
+++ JPHacks2019-firstchecklist/VMforBeginner/VMforBeginnerApp/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+from django.urls import path
+
+urlpatterns = [
+    url(r'^$', views.Home.as_view(), name='home'),
+    # url(r'^checklist/', views.Home.as_view(), name='home'),
+    # path('checklist/', views.demo3, name='demo03'),
+]
