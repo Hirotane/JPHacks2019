ae970cf762be66cf48ea34e2d47b77e915cb786d
Hirotane <56142746+Hirotane@users.noreply.github.com>
2019-10-20 10:04:43 +0900

    add UI

---
+++ VMforBeginner/VMforBeginnerApp/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+from django.urls import path
+
+urlpatterns = [
+    url(r'^$', views.start, name='start'),
+    path('all/', views.all, name='all'),
+    path('home1/', views.Home.as_view(), name='home1'),
+    # url(r'^checklist/', views.Home.as_view(), name='home'),
+    path('checklist/', views.demo3, name='demo03'),
+    path('home2/', views.home2, name='home2'),
+    path('home3/', views.home3, name='home3'),
+]
